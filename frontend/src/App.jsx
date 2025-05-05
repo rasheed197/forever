dc1f115524a92d89da88cb007e305fab3df5afa9
rasheed197 <tiamiyurasheed.tr@gmail.com>
2025-05-05 14:39:08 +0100

    completed completed page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
@@ -17,6 +18,7 @@ import SearchBar from "./components/SearchBar";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
